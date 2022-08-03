cd40c6e2f59e119708356c0a2c85d0c8bc8e9699
OkenHaha <keithellakpamoken@gmail.com>
2022-08-03 15:08:20 +0530

    removed commented code

--- index.js
+++ index.js
@@ -4,12 +4,3 @@ const navLink = document.querySelector('.menu');
 hamburger.addEventListener('click', () => {
   navLink.classList.toggle('show');
 });
-
-
-/*
-trigger.addEventListener("click", toggleModal);
-function closeButton() {
-addEventListener("click", toggleModal);
-}
-window.addEventListener("click", windowOnClick);
-*/
